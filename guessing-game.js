6c51f85d500453d5e0fbffa197b691c4539bef0f
Kate Vong <katiejnete@gmail.com>
2024-08-28 17:50:58 -0700

    complete guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,20 @@
-function guessingGame() {
-
+function guessingGame(guess = null) {
+  let guessCount = 0;
+  let isGameOver = false;
+  const secretNum = Math.floor(Math.random() * 100);
+  return function (guess) {
+    if (isGameOver) return "The game is over, you already won!";
+    if (guess !== secretNum) {
+      guessCount++;
+      return guess < secretNum
+        ? `${guess} is too low!`
+        : `${guess} is too high!`;
+    } else {
+      guessCount++;
+      isGameOver = true;
+      return `You win! You found ${guess} in ${guessCount} guesses.`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
